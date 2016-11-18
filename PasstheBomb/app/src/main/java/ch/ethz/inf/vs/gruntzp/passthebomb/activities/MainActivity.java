950eb280c8a5e7f3273cc0cd1488ad7b072aeaec
saphiraflame <saphiraflame@gmail.com>
2016-11-18 16:52:59 +0100

    - Added all necessary components to MainActivity.java for it to function - Made it show a toast if username field is empty - Gave certain activities their proper label

--- PasstheBomb/app/src/main/java/ch/ethz/inf/vs/gruntzp/passthebomb/activities/MainActivity.java
+++ PasstheBomb/app/src/main/java/ch/ethz/inf/vs/gruntzp/passthebomb/activities/MainActivity.java
@@ -1,13 +1,51 @@
 package ch.ethz.inf.vs.gruntzp.passthebomb.activities;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.inputmethod.EditorInfo;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText mEdit;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mEdit   = (EditText)findViewById(R.id.text_field);
+
+        //TODO: save the username in the text field -> use preferences or something
+    }
+
+    public void onClickCreate(View view) {
+        if(mEdit.getText().toString().isEmpty()) {
+            Toast toast = Toast.makeText(this, R.string.username_required, Toast.LENGTH_SHORT);
+            toast.show();
+        }else{
+            Intent myIntent = new Intent(this, CreateActivity.class);
+            myIntent.putExtra("creator_name", mEdit.getText().toString());
+            this.startActivity(myIntent);
+        }
+    }
+
+    public void onClickJoin(View view) {
+        if(mEdit.getText().toString().isEmpty()) {
+            Toast toast = Toast.makeText(this, R.string.username_required, Toast.LENGTH_SHORT);
+            toast.show();
+        }else{
+            Intent myIntent = new Intent(this, JoinActivity.class);
+            myIntent.putExtra("player_name", mEdit.getText().toString());
+            this.startActivity(myIntent);
+        }
+    }
+
+    public void onClickTutorial(View view) {
+        Intent myIntent = new Intent(this, TutorialActivity.class);
+        this.startActivity(myIntent);
     }
 }
