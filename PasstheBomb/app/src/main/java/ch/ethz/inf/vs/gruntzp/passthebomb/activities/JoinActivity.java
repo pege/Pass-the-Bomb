38013b2a799835c72e5a8549ae03c755d8826de8
QSshellon <qs.michelle.woon@gmail.com>
2016-11-18 23:41:35 +0100

    added refresh button in JoinActivity.java

--- PasstheBomb/app/src/main/java/ch/ethz/inf/vs/gruntzp/passthebomb/activities/JoinActivity.java
+++ PasstheBomb/app/src/main/java/ch/ethz/inf/vs/gruntzp/passthebomb/activities/JoinActivity.java
@@ -2,6 +2,7 @@ package ch.ethz.inf.vs.gruntzp.passthebomb.activities;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class JoinActivity extends AppCompatActivity {
 
@@ -9,5 +10,12 @@ public class JoinActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_join);
+
+        //TODO add list of games
+    }
+
+    //TODO implement this
+    public void onClickRefresh(View view) {
+
     }
 }
