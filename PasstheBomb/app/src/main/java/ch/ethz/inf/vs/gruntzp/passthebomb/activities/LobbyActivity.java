aac6a6dec9381933a8381ef5463e25e77d65287c
QSshellon <qs.michelle.woon@gmail.com>
2016-11-20 01:54:16 +0100

    made startButton in LobbyActivity.java only visible to the creator of the game

--- PasstheBomb/app/src/main/java/ch/ethz/inf/vs/gruntzp/passthebomb/activities/LobbyActivity.java
+++ PasstheBomb/app/src/main/java/ch/ethz/inf/vs/gruntzp/passthebomb/activities/LobbyActivity.java
@@ -4,16 +4,22 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
 import ch.ethz.inf.vs.gruntzp.passthebomb.gamelogic.Game;
 
 public class LobbyActivity extends AppCompatActivity {
 
     private int numberOfPlayers; //TODO count players
+    private Button startButton;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_lobby);
+
+        startButton = (Button) findViewById(R.id.start_game);
+
         setLobbyTitle();
         setStartButton();
 
@@ -28,7 +34,14 @@ public class LobbyActivity extends AppCompatActivity {
     }
 
     private void setStartButton(){
-
+        Bundle extras = getIntent().getExtras();
+        Boolean isCreator = extras.getBoolean("creator");
+        if (isCreator){
+            startButton.setVisibility(View.VISIBLE);
+        } else
+        {
+            startButton.setVisibility(View.INVISIBLE);
+        }
     }
 
     /* Starts the game.
